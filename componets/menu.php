196278df5735494f5a322ddaa760c48b432c2932
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2024-01-18 23:08:28 +0530

    Add files via upload

---
+++ componets/menu.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="index.php">home</a></li>
+    <li><a href="?a=student_add">admission</a></li>
+    <li><a href="?a=enroll">courses</a><li>
+    <li><a href="?a=view">students</a></li>
+    <li><a href="?a=view_courses">enrolled</a></li>
+    <li><a href="">contact us</a></li>
+</ul>
